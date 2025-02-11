13b1a3dbdb648856a2661168e457f18182486018
rohis <63966639+Nix4444@users.noreply.github.com>
2025-02-11 23:43:37 +0530

    setting up agent

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
 const app = express()
-
-
+import {openaiApiKey} from "@repo/agent/openaiApiKey";
+console.log(openaiApiKey);
 app.listen(3001);
---
+++ packages/agent/src/index.ts
@@ -0,0 +1,3 @@
+import dotenv from "dotenv";
+dotenv.config();
+export const openaiApiKey = process.env.OPENAI_API_KEY || "";
