60c6ef1b6f8ace70e7fe9b695769806eeb8646c6
rohis <63966639+Nix4444@users.noreply.github.com>
2025-03-05 18:44:42 +0530

    removed redundant endpoints

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -12,36 +12,14 @@ app.use(
 );
 app.use(express.json());
 
-app.post("/", async (req, res) => {
+app.post("/chain/generate", async (req, res) => {
   try {
     const body = req.body;
     const question = body.question;
     const response = await unifiedQueryChain(question);
-    res.json({ response });
+    res.json({ message: response.finalresponse });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Please ask relevant questions." });
-  }
-});
-
-let database_name:string [] =[]
-app.post("/database", async (req, res) => {
-  try {
-    const body = req.body;
-    database_name.push(body.database)
-    console.log(database_name)
-    res.send("Received!");
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-app.get("/databaseinfo", async (req, res) => {
-  try {
-    res.json({database:database_name});
-  } catch (error) {
-    console.error("Error in /databaseinfo:", error);
-    res.status(500).send("Internal Server Error");
+    res.status(500).json({message: "Please ask relevant questions to your database"});
   }
 });
 
